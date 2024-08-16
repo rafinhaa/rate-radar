34ceb2bb737852bd991a7e6794a92d4d048a6ea6
Rafinhaa <rafinhaa@me.com>
2024-08-16 19:08:49 -0300

    feat: multi languages

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,18 @@
 import "@testing-library/react-native/extend-expect"
+
+import { resources as mockResources } from "./src/utils/tests/index"
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (path: string) =>
+      path
+        .split(".")
+        .reduce((acc: any, key: any) => acc && acc[key], mockResources),
+  }),
+}))
+
+jest.mock("i18next", () => ({
+  use: jest.fn().mockReturnValue({
+    init: jest.fn(),
+  }),
+}))
