de9489a10c30be718ce733da4f0a568091e026f1
Rafinhaa <rafinhaa@me.com>
2024-08-20 11:38:33 -0300

    feat: style with nativewind

--- jest.config.ts
+++ jest.config.ts
@@ -5,4 +5,7 @@ export default {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
+  moduleNameMapper: {
+    "^.+\\.(css|less)$": "<rootDir>/__mocks__/global.css.js",
+  },
 }
